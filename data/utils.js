4fd818071285d6d6518c7bbc583d94e38ad55be7
蔡佳男 <xalley@qq.com>
2015-12-11 20:49:49 +0800

    拆分 pycall 到 utils.js

--- data/utils.js
+++ data/utils.js
@@ -1,1 +1,37 @@
+function pycall(name) {
+    var kwargs = arguments[1] || {}
+    return JSON.parse(
+        prompt(name,
+            JSON.stringify(kwargs)
+        )
+    )
+}
+    
+function open(editor) {
+    ret = pycall('open')
+    if (ret.success) {
+        editor.value(ret.data)
+    } else {
+        alert(ret.errmess)
+    }
+}
 
+function save(editor) {
+    // 判断行末 添加换行符号
+    ret = pycall('save', {
+        data: editor.value()
+    })
+    if (!ret.success) {
+        alert(ret.errmess)
+    }
+}
+
+function save_as(editor) {
+    // 判断行末 添加换行符号
+    ret = pycall('save_as', {
+        data: editor.value()
+    })
+    if (!ret.success) {
+        alert(ret.errmess)
+    }
+}
